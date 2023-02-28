96cab08d263528c080e88a8720622574ca277e80
Pheon-Dev <devpheon@gmail.com>
2023-02-28 22:20:51 +0300

    fix: connection address error

--- src/main.rs
+++ src/main.rs
@@ -9,15 +9,19 @@ use warp::ws::{Message, WebSocket};
 use warp::Filter;
 
 static NEXT_USERID: std::sync::atomic::AtomicUsize = std::sync::atomic::AtomicUsize::new(1);
+
 type Users = Arc<RwLock<HashMap<usize, mpsc::UnboundedSender<Result<Message, warp::Error>>>>>;
+
 #[tokio::main]
 async fn main() {
-    let address = env::args()
+    let addr = env::args()
         .nth(1)
-        .unwrap_or_else(|| "127.0.0.1::8080".to_string());
-    let socket_address: SocketAddr = address.parse().expect("Valid Socket Address");
+        .unwrap_or_else(|| "127.0.0.1:8080".to_string());
+    let socket_address: SocketAddr = addr.parse().expect("valid socket Address");
+
     let users = Users::default();
     let users = warp::any().map(move || users.clone());
+
     let opt = warp::path::param::<String>()
         .map(Some)
         .or_else(|_| async { Ok::<(Option<String>,), std::convert::Infallible>((None,)) });
@@ -30,29 +34,33 @@ async fn main() {
             format!("Hello, {}!", name.unwrap_or_else(|| "world".to_string()))
         });
 
-    //GET /ws
+    // GET /ws
     let chat = warp::path("ws")
         .and(warp::ws())
         .and(users)
         .map(|ws: warp::ws::Ws, users| ws.on_upgrade(move |socket| connect(socket, users)));
 
     let files = warp::fs::dir("./static");
+
     let res_404 = warp::any().map(|| {
         warp::http::Response::builder()
             .status(warp::http::StatusCode::NOT_FOUND)
-            .body(fs::read_to_string("./static/404.html").expect("404"))
+            .body(fs::read_to_string("./static/404.html").expect("404 404?"))
     });
 
     let routes = chat.or(hello).or(files).or(res_404);
+
     let server = warp::serve(routes).try_bind(socket_address);
-    println!("Running server at {}!", address);
+
+    println!("Running server at {}!", addr);
+
     server.await
 }
 
 async fn connect(ws: WebSocket, users: Users) {
     // Bookkeeping
     let my_id = NEXT_USERID.fetch_add(1, std::sync::atomic::Ordering::Relaxed);
-    println!("Welcome user {}", my_id);
+    println!("Welcome User {}", my_id);
 
     // Establishing a connection
     let (user_tx, mut user_rx) = ws.split();
@@ -63,6 +71,7 @@ async fn connect(ws: WebSocket, users: Users) {
     tokio::spawn(rx.forward(user_tx));
     users.write().await.insert(my_id, tx);
 
+    // Reading and broadcasting messages
     while let Some(result) = user_rx.next().await {
         broadcast_msg(result.expect("Failed to fetch message"), &users).await;
     }
